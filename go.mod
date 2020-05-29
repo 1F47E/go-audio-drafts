0605e4b5c774097c1a37ed17078404fa4fabc217
К <kkkaspar@pm.me>
2020-05-30 01:30:42 +0700

    added audio examples, go mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/go-audio-drafts
+
+go 1.14
+
+require github.com/go-audio/audio v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
+import "fmt"
+import "github.com/go-audio/audio"
+
+var (
+	sampleRate  = 48000
+	numChannels = 1
+	bitDepth    = 16
+)
+
+
 func ogg2wav(fileIn, fileOut string) error {
+
+	audioBuffer := audio.IntBuffer{
+		Format: &audio.Format{
+			NumChannels: numChannels,
+			SampleRate:  sampleRate,
+		},
+		SourceBitDepth: bitDepth,
+	}
+	fmt.Printf("audioBuffer len: %d\n", len(audioBuffer.Data))
 
 	return nil
 }
