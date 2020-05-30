92ec15ca27ef53c90fbfe6d837a739d6d9edea32
К <kkkaspar@pm.me>
2020-05-31 01:43:16 +0700

    ogg2wav decoding draft

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/go-audio-drafts
 
 go 1.14
 
-require github.com/go-audio/audio v1.0.0 // indirect
+require (
+	github.com/go-audio/audio v1.0.0 // indirect
+	github.com/go-audio/wav v1.0.0 // indirect
+	github.com/hraban/opus v0.0.0-20191117073431-57179dff69a6 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
-import "github.com/go-audio/audio"
+import (
+	"fmt"
+	"github.com/go-audio/audio"
+	"io"
+	"os"
+	"github.com/hraban/opus"
+	"github.com/go-audio/wav"
+)
 
 var (
 	sampleRate  = 48000
@@ -9,9 +15,30 @@ var (
 	bitDepth    = 16
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
-func ogg2wav(fileIn, fileOut string) error {
+func ogg2wav(fileIn, fileOut string) {
+	file_in, err := os.Open(fileIn)
+	check(err)
+	stream, err := opus.NewStream(file_in)
+	check(err)
+	defer stream.Close()
 
+	// create wav buffer
+	file_out, err := os.Create(fileOut)
+	check(err)
+
+	// wav encoder
+	encoder_out := wav.NewEncoder(file_out, sampleRate, bitDepth, numChannels, 1)
+
+	frameSize := numChannels * 60 * sampleRate / 1000
+	oggBuffer := make([]int16, frameSize)
+
+	// create audio.IntBuffer
 	audioBuffer := audio.IntBuffer{
 		Format: &audio.Format{
 			NumChannels: numChannels,
@@ -19,9 +46,35 @@ func ogg2wav(fileIn, fileOut string) error {
 		},
 		SourceBitDepth: bitDepth,
 	}
-	fmt.Printf("audioBuffer len: %d\n", len(audioBuffer.Data))
 
-	return nil
+	for {
+		cnt, err := stream.Read(oggBuffer)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			check(err)
+		}
+		pcm := oggBuffer[:cnt*numChannels]
+
+
+		// TODO write actual decoded data
+		for _, b := range pcm {
+			audioBuffer.Data = append(audioBuffer.Data, int(b))
+		}
+
+
+	}
+	fmt.Printf("audioBuffer len: %d\n", len(audioBuffer.Data))
+	// Write buffer to output file. This writes a RIFF header and the PCM chunks from the audio.IntBuffer.
+
+	if err := encoder_out.Write(&audioBuffer); err != nil {
+		check(err)
+	}
+	// close file + write header
+	if err := encoder_out.Close(); err != nil {
+		check(err)
+	}
+	fmt.Printf("encoder_out WrittenBytes: %d\n", encoder_out.WrittenBytes)
 }
 
 func wav2mp3(fileIn, fileOut string) error {
